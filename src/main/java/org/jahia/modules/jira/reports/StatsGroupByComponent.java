e8b98d9c1587301fd3054f2d3af8fd93583a4482
Michael de Checchi <mdechecchi@jahia.com>
2018-11-29 15:43:47 +0100

    Optimize the creation of components' map + add component version if available + add list of components

--- src/main/java/org/jahia/modules/jira/reports/StatsGroupByComponent.java
+++ src/main/java/org/jahia/modules/jira/reports/StatsGroupByComponent.java
@@ -7,54 +7,68 @@ import com.atlassian.jira.issue.statistics.StatisticsMapper;
 import com.atlassian.jira.issue.statistics.StatsGroup;
 import com.atlassian.jira.project.version.Version;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class StatsGroupByComponent extends StatsGroup {
 
-    // Map<Key, Value> => <Version, List of issues>
     private Long selectedProjectId = 0L;
-
-    // TODO Current Map is handled by StatsGroup (Version/Issues list) but we should change it to prevent too much filtering
-    // Map<Version, Map<ProjectComponent, Collection<Issue>>> componentsMap;
+    private Map<Version, Map<ProjectComponent, Collection<Issue>>> componentsMap;
+    private Map<Version, Collection<Issue>> issuesWithoutComponents;
 
     public StatsGroupByComponent(StatisticsMapper<?> mapper, Long projectId) {
         super(mapper);
         selectedProjectId = projectId;
     }
 
-    public Collection<ProjectComponent> getComponents() {
-        return ComponentAccessor.getProjectComponentManager().findAllForProject(selectedProjectId);
+    public void buildComponentMap(Version version) {
+        componentsMap = new HashMap<Version, Map<ProjectComponent, Collection<Issue>>>();
+        issuesWithoutComponents = new HashMap<Version, Collection<Issue>>();
+        issuesWithoutComponents.put(version, new ArrayList<Issue>());
+
+        Map<ProjectComponent, Collection<Issue>> map = new HashMap<ProjectComponent, Collection<Issue>>();
+        Iterator<Issue> it = ((Collection<Issue>) this.get(version)).iterator();
+        while (it.hasNext()) {
+            Issue issue = it.next();
+
+            if (issue.getComponents().size() > 0) {
+                // Only map the first component with the issue
+                Iterator<ProjectComponent> componentIterator = issue.getComponents().iterator();
+                ProjectComponent component = componentIterator.next();
+                if (map.containsKey(component)) {
+                    map.get(component).add(issue);
+                } else {
+                    map.put(component, new ArrayList<Issue>(){{add(issue);}});
+                }
+            } else {
+                issuesWithoutComponents.get(version).add(issue);
+            }
+        }
+        componentsMap.put(version, map);
     }
 
-    // TODO with Jira > 7.0 replace getComponentObjects by getComponents
-    @SuppressWarnings("unchecked")
+    public Collection<ProjectComponent> getComponents(Version version) {
+        return componentsMap.get(version).keySet();
+    }
+
     public Collection<Issue> getIssuesByComponent(Version version, ProjectComponent component) throws Exception {
-        Collection<Issue> restrictedList = new ArrayList<Issue>();
-        Collection<Issue> issues = (Collection<Issue>) this.get(version);
-        Iterator<Issue> it = issues.iterator();
-        while (it.hasNext()) {
-            Issue issue = (Issue) it.next();
-            if (issue.getComponents().contains(component)) {
-                restrictedList.add(issue);
-            }
-        }
-        return restrictedList;
+        return componentsMap.get(version).get(component);
     }
 
-    // TODO with Jira > 7.0 replace getComponentObjects by getComponents
-    @SuppressWarnings("unchecked")
     public Collection<Issue> getIssuesWithoutComponent(Version version) {
-        Collection<Issue> restrictedList = new ArrayList<Issue>();
-        Collection<Issue> issues = (Collection<Issue>) this.get(version);
-        Iterator<Issue> it = issues.iterator();
-        while (it.hasNext()) {
-            Issue issue = (Issue) it.next();
-            if (issue.getComponents().isEmpty()) {
-                restrictedList.add(issue);
+        return issuesWithoutComponents.get(version);
+    }
+
+    public Version getVersionOfComponent(ProjectComponent component){
+        // Get all the versions remaining except the selected one
+        Collection<Version> versions = ComponentAccessor.getVersionManager().getVersionsUnreleased(selectedProjectId, false);
+
+        // Get a version matching the component name
+        for (Version version:versions) {
+            if (version.getName().contains(component.getName())) {
+                return version;
             }
         }
-        return restrictedList;
+
+        return null;
     }
 }
