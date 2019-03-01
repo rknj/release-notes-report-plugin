95261b3b37a7a5e36ee1a7198a5c8d82551dbad1
Michael de Checchi <mdechecchi@jahia.com>
2019-03-01 17:55:09 +0100

    Add all the other fix versions different from the current one and order the versions by name in the dropdown

--- src/main/java/org/jahia/modules/jira/reports/VersionOptionalValuesGenerator.java
+++ src/main/java/org/jahia/modules/jira/reports/VersionOptionalValuesGenerator.java
@@ -28,7 +28,15 @@ public class VersionOptionalValuesGenerator implements ValuesGenerator<Object>
             VersionManager versionManager = ComponentAccessor.getVersionManager();
             I18nHelper i18n = new I18nBean(remoteUser);
 
-            Collection<Version> unreleasedVersions = versionManager.getVersionsUnreleased(projectGV.getLong("id"), false);
+            Collection<Version> unreleasedVersionsCollection = versionManager.getVersionsUnreleased(projectGV.getLong("id"), false);
+            List<Version> unreleasedVersions = new ArrayList<Version>( unreleasedVersionsCollection );
+            Collections.sort(unreleasedVersions, new Comparator<Version>( ){
+                @Override
+                public int compare(Version o1, Version o2) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+
             Map<Long, String> unreleased = ListOrderedMap.decorate(new HashMap(unreleasedVersions.size()));
             Iterator<Version> unreleasedIter = unreleasedVersions.iterator();
             if (unreleasedIter.hasNext())
@@ -37,27 +45,37 @@ public class VersionOptionalValuesGenerator implements ValuesGenerator<Object>
                 while (unreleasedIter.hasNext())
                 {
                     Version version = unreleasedIter.next();
-                    unreleased.put(version.getId(), "- " + version.getName());
+                    unreleased.put(version.getId(), "    " + version.getName());
                 }
             }
 
-            Map<Long, String> released = ListOrderedMap.decorate(new HashMap(unreleasedVersions.size()));
-            List<Version> releasedIter = new ArrayList(versionManager.getVersionsReleased(projectGV.getLong("id"), false));
-            if (!releasedIter.isEmpty())
+
+
+            Collection<Version> releasedVersionsCollection = versionManager.getVersionsReleased(projectGV.getLong("id"), false);
+            List<Version> releasedVersions = new ArrayList<Version>( releasedVersionsCollection );
+            Collections.sort(releasedVersions, new Comparator<Version>( ){
+                @Override
+                public int compare(Version o1, Version o2) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+
+            Map<Long, String> released = ListOrderedMap.decorate(new HashMap(releasedVersions.size()));
+            Iterator<Version> releasedIter = releasedVersions.iterator();
+            if (releasedIter.hasNext())
             {
                 released.put(new Long(-3), i18n.getText("common.filters.releasedversions"));
-                Collections.reverse(releasedIter);
-                for (Iterator<Version> iterator = releasedIter.iterator(); iterator.hasNext();)
+                while (releasedIter.hasNext())
                 {
-                    Version version = iterator.next();
-                    released.put(version.getId(), "- " + version.getName());
+                    Version version = releasedIter.next();
+                    released.put(version.getId(), "    " + version.getName());
                 }
             }
 
             int size = unreleased.size() + released.size() + 1;
             Map<Long, String> versions = ListOrderedMap.decorate(new HashMap(size));
 
-            versions.put(new Long(-1), i18n.getText("timetracking.nofixversion"));
+            //versions.put(new Long(-1), i18n.getText("timetracking.nofixversion"));
             versions.putAll(unreleased);
             versions.putAll(released);
             return versions;
